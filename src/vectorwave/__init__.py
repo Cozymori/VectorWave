5cb08d4437dc4d72f32f57473177796c79173c0f
Jun Yeong Kim <junyeonggim5@gmail.com>
2025-11-22 08:23:25 +0900

    build(init): Add generate_and_register_metadata to init
    For better experience, add it to main init
    
    Signed-off-by: Jun Yeong Kim <junyeonggim5@gmail.com>

--- src/vectorwave/__init__.py
+++ src/vectorwave/__init__.py
@@ -4,6 +4,7 @@ from .database.db import initialize_database
 from .database.db_search import search_functions, search_executions, search_errors_by_message, search_functions_hybrid
 from .monitoring.tracer import trace_span
 from .search.rag_search import search_and_answer, analyze_trace_log
+from .core.generator import generate_and_register_metadata
 
 __all__ = [
     'vectorize',
@@ -14,5 +15,6 @@ __all__ = [
     'search_errors_by_message',
     'trace_span',
     'search_and_answer',
-    'analyze_trace_log'
+    'analyze_trace_log',
+    'generate_and_register_metadata'
 ]
