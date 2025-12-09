12212d7a57ceb2286db36cb464db27a48275ecb8
Jun Yeong Kim <junyeonggim5@gmail.com>
2025-12-09 16:35:38 +0900

    build(init): Add auto injector to init functions.
    ditto
    
    Signed-off-by: Jun Yeong Kim <junyeonggim5@gmail.com>

--- src/vectorwave/__init__.py
+++ src/vectorwave/__init__.py
@@ -1,5 +1,4 @@
 from .core.decorator import vectorize
-
 from .database.db import initialize_database
 from .database.db_search import search_functions, search_executions, search_errors_by_message, search_functions_hybrid
 from .monitoring.tracer import trace_span
@@ -9,6 +8,7 @@ from .utils.healer import VectorWaveHealer
 from .utils.replayer import VectorWaveReplayer
 from .utils.replayer_semantic import SemanticReplayer
 from .database.dataset import VectorWaveDatasetManager
+from .core.auto_injector import VectorWaveAutoInjector
 
 __all__ = [
     'vectorize',
@@ -24,5 +24,6 @@ __all__ = [
     'VectorWaveHealer',
     'VectorWaveReplayer',
     'SemanticReplayer',
-    'VectorWaveDatasetManager'
+    'VectorWaveDatasetManager',
+    'VectorWaveAutoInjector'
 ]
